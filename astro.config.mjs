11266af59811bfbf978b823b86c6e3fdc1ace3c8
dot <dot@x3c.ca>
2026-03-29 12:24:08 -0700

    2026

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,22 @@
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+const reactClientEntrypoint = fileURLToPath(
+  new URL('./node_modules/@astrojs/react/dist/client.js', import.meta.url)
+);
+const reactServerEntrypoint = fileURLToPath(
+  new URL('./node_modules/@astrojs/react/dist/server.js', import.meta.url)
+);
+
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    resolve: {
+      alias: {
+        '@astrojs/react/client.js': reactClientEntrypoint,
+        '@astrojs/react/server.js': reactServerEntrypoint,
+      },
+    },
+  },
 });
